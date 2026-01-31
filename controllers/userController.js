9e77460d799ac2ac2af05d686992d6dec6a0eb42
Asiru <asirudilshan44@gmail.com>
2026-01-31 13:21:39 +0530

    get all users✏️

--- controllers/userController.js
+++ controllers/userController.js
@@ -176,13 +176,14 @@ export async function updateUser(req,res){
         message:"Details update successfuly"
        })
     }else{
-    const { id, age, gender, birthday } = req.body
+    const { id, age, gender, birthday,img } = req.body
 
     const updatingData= {
       id,
       age,
       gender,
       birthday,
+      img
     }
 
          await User.updateOne({userName:userName},updatingData)
@@ -197,6 +198,76 @@ export async function updateUser(req,res){
     }catch(err){
         res.status(500).json({
             message:"internal server error",
+            error:err
+        })
+    }
+}
+export function isAdmin(req){
+    if (req.user == null){
+        return false
+    }
+
+    if (req.user.role != "admin"){
+        return false
+ 
+    }
+
+    return true
+
+}
+
+export async function getUser(req,res){
+    const userName=req.params.userName
+    try{
+        if(req.user==null){
+            return
+        }
+  
+        const user=await User.findOne(
+            {userName:userName}
+        )
+
+        if(user==null){
+            res.status(404).json({
+                message:"user not found"
+            })
+            return
+        }
+        if(user.isBlocked==false && user.role != "admin" && user.role != "madam" ){
+            res.json(user)
+        }
+        else{
+            if(!isAdmin(req)){
+                res.status(404).json({
+                    message:"user not found"
+                })
+            }else{
+                res.json(user)
+            }
+        }
+
+}catch(err){
+    res.status(500).json({
+        message:"internal error",
+        error:err
+    })
+}
+
+}
+
+export async function getAllUsers(req,res) {
+    try{
+      if(isAdmin(req)){
+        const user=await User.find()
+        res.json(user)
+      }else{
+        res.status(403).json({
+            message:"not authorized"
+        })
+      }
+    }catch(error){
+        res.json({
+            message:"failed to get Users",
             error:err
         })
     }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createUser, loginUser, updateUser } from "../controllers/userController.js";
+import { createUser, getAllUsers, getUser, loginUser, updateUser } from "../controllers/userController.js";
 
 const userRouter= express.Router();
 
 userRouter.post("/",createUser)
 userRouter.post("/login",loginUser)
 userRouter.put("/",updateUser)
+userRouter.get("/:userName",getUser)
+userRouter.get("/",getAllUsers)
 
 export default userRouter;
